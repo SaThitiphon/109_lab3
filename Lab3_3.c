f4ac5bf57cc14d98d5b2b87cfc09bebe10210632
Thitipon <thitiphon.s@ku.th>
2021-02-15 17:22:20 +0700

    Update Lab3_3.c

--- Lab3_3.c
+++ Lab3_3.c
@@ -5,14 +5,17 @@ main ()
 {
   int n, x = 2;
   scanf ("%d", &n);
-  while (n != 1)
+  if (n >= 1 && n <= 1000)
+    {
+      while (n != 1)
     {
       while (n % x == 0)
-    {
-      printf ("%d\n", x);
-      n = n / x;
-    }
+        {
+          printf ("%d\n", x);
+          n = n / x;
+        }
       x++;
+    }
     }
   printf ("0");
   return 0;

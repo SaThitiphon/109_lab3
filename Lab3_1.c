6a915ca68a4d6c2cb3deaaf0ab86dce8703c75dd
Thitipon <thitiphon.s@ku.th>
2021-02-15 16:49:40 +0700

    ver1

---
+++ Lab3_1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+    int a[5],b[5],c[5],d[5],e[5],i,s[5],n,j;
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",a[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",b[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",c[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",d[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",e[i]);
+    }
+    s[0] = a[0]+a[1]+a[2]+a[3]+a[4];
+    s[1] = b[0]+b[1]+b[2]+b[3]+b[4];
+    s[2] = c[0]+c[1]+c[2]+c[3]+c[4];
+    s[3] = d[0]+d[1]+d[2]+d[3]+d[4];
+    s[4] = e[0]+e[1]+e[2]+e[3]+e[4];
+    n = s[0];
+    for(j=1;j<5;j++)
+    {
+        if(n<s[j])
+        {
+            n = s[j];
+            i = j+1;
+        }
+    }
+    printf("%d %d",i,j);
+    
+}

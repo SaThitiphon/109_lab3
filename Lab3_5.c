bd3f482b410ef401628e4d1b1b2887b5ffb605e0
Thitipon <thitiphon.s@ku.th>
2021-02-15 18:40:41 +0700

    luckynumber

---
+++ Lab3_5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+  
+    int n,j=0,a[3],k,b,m;
+    scanf("%d",&m);
+    n = m;
+    while(n>1){
+        for(int i = 2; i<=n; i++)
+        {
+            if(n%i==0){
+                b = i;
+                n = n/i;
+                break;
+            }
+        }
+        a[j] = b;
+        j++;
+    }
+    if(j==3)
+    {
+        for(k=0;k<3;k++)
+        {
+            if(a[k]==a[k+1])
+            {
+                printf("%d is not a Lucky Number.",m);
+            }
+        }
+        if(a[0]!=a[1]&&a[0]!=a[2]&&a[1]!=a[2])
+        {
+            printf("%d is a Lucky Number.",m);
+        }
+    }
+    return 0;
+
+}
